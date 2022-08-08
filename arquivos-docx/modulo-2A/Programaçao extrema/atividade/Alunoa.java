42f1e864253670b8712a9282a20edb0c77db5358
Adriano Weverton <adriwco@gmail.com>
2022-08-08 03:01:52 -0300

    Delete arquivos-docx directory

--- arquivos-docx/modulo-2A/Programaçao extrema/atividade/Alunoa.java
+++
@@ -1,7 +0,0 @@
-public class Alunoa {
-  public static void main(String[] args) {
-    Aluno aluno = new Aluno("Adriano", "Weverton", 21);
-    aluno.setIdade(67);
-    System.out.println(aluno.getNome() + " " + aluno.getSobrenome() + ", " + aluno.getIdade());
-  }
-}
--- arquivos-docx/modulo-2A/Programaçao extrema/atividade/sobreposicao_de_metodos/LivroAbstrato.java
+++
@@ -1,7 +0,0 @@
-package sobreposicao_de_metodos;
-
-public abstract class LivroAbstrato {
-  public void ler() {
-    System.out.println("Lendo Livro");
-  }
-}
