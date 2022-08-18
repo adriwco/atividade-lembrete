ceb4698508dbba852b27824f21b4f451670451ea
Adriano Weverton <adriwco@gmail.com>
2022-08-18 20:33:58 -0300

    Add files via upload

--- aplicacaoFila/mainFila.java
+++ aplicacaoFila/mainFila.java
@@ -2,17 +2,13 @@ package aplicacaoFila;
 
 import javax.swing.JOptionPane;
 
-//Situação: algoritmo que recebe do usuário dez números inteiros para armazenar apenas os números pares numa Fila com capacidade de dez elementos e mostra os números da Fila.
-
 public class mainFila {
   public static void main(String[] args) {
-    fila intFila = new fila(10);
+    fila intFila = new fila(5);
     int entrada = 0;
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 5; i++) {
       entrada = Integer.parseInt(JOptionPane.showInputDialog("Digite o " + i + "° valor inteiro: "));
-      if (entrada % 2 == 0) {
-        intFila.enfileirar(entrada);
-      }
+      intFila.enfileirar(entrada);
     }
     intFila.mostrarFila();
     System.exit(0);
