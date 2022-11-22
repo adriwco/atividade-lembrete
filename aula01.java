7e0aa46a16076411849b919e29769cbfa1ea67fd
Adriano Weverton <adriwco@gmail.com>
2022-11-22 03:00:45 -0300

    Delete aula01.java

--- aula01.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-class aula01{
-  public static void main(String args[]){
-    String nome;
-    nome = JOptionPane.showInputDialog("Digite o seu nome:");
-    JOptionPane.showMessageDialog(null,"Nome digitado:\n"+nome);
-    System.exit(0);
-  }
-}
