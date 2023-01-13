b5929f502201051cbcfa8cd7530ff7c48dee56d9
adriwco <adriwco@gmail.com>
2023-01-13 01:10:14 -0300

    Traversing e Manipulação

--- JS_ES6+/2_dom_iniciante/7_traversing_manipulacao/script.js
+++ JS_ES6+/2_dom_iniciante/7_traversing_manipulacao/script.js
@@ -4,14 +4,14 @@
 OUTERHTML, INNERHTML E INNERTEXT: Propriedades que retornam uma string contendo o html ou texto. É possível atribuir um novo valor para as mesmas element.innerText = 'Novo Texto'.
 */
 
-const menu = document.querySelector('.menu');
+const menu = document.querySelector(".menu");
 
 menu.outerHTML; // todo o html do elemento
 menu.innerHTML; // html interno
 menu.innerText; // texto, sem tags
 
-menu.innerText = '<p>Texto</p>'; // a tag vai como texto
-menu.innerHTML = '<p>Texto</p>'; // a tag é renderizada
+menu.innerText = "<p>Texto</p>"; // a tag vai como texto
+menu.innerHTML = "<p>Texto</p>"; // a tag é renderizada
 
 // --------------------------------------------------------------------------------------------
 
@@ -19,7 +19,7 @@ menu.innerHTML = '<p>Texto</p>'; // a tag é renderizada
 TRASVERSING: Como navegar pelo DOM, utilizando suas propriedades e métodos.
 */
 
-const lista0 = document.querySelector('.animais-lista');
+const lista0 = document.querySelector(".animais-lista");
 
 lista0.parentElement; // pai
 lista0.parentElement.parentElement; // pai do pai
@@ -30,8 +30,8 @@ lista0.children; // HTMLCollection com os filhos
 lista0.children[0]; // primeiro filho
 lista0.children[--lista0.children.length]; // último filho
 
-lista0.querySelectorAll('li'); // todas as LI's
-lista0.querySelector('li:last-child'); // último filho
+lista0.querySelectorAll("li"); // todas as LI's
+lista0.querySelector("li:last-child"); // último filho
 
 // --------------------------------------------------------------------------------------------
 
@@ -39,7 +39,7 @@ lista0.querySelector('li:last-child'); // último filho
 ELEMENT VS NODE: Element's represetam um elemento html, ou seja, uma tag. Node representa um nó, e pode ser um elemento (Element), texto, comentário, quebra de linha e mais.
 */
 
-const lista1 = document.querySelector('.animais-lista');
+const lista1 = document.querySelector(".animais-lista");
 
 lista1.previousElementSibling; // elemento acima
 lista1.previousSibling; // node acima
@@ -54,9 +54,9 @@ lista1.childNodes; // todos os node child
 /*
 MANIPULANDO ELEMENTOS: É possível mover elementos no dom com métodos de Node.
 */
-const lista2 = document.querySelector('.animais-lista');
-const contato = document.querySelector('.contato');
-const titulo = contato.querySelector('.titulo');
+const lista2 = document.querySelector(".animais-lista");
+const contato = document.querySelector(".contato");
+const titulo = contato.querySelector(".titulo");
 
 contato.appendChild(lista2); // move lista para o final de contato
 contato.insertBefore(lista2, titulo); // insere a lista antes de titulo
@@ -69,11 +69,11 @@ contato.replaceChild(lista2, titulo); // substitui titulo por lista
 NOVOS ELEMENTOS: Podemos criar novos elementos com o método createElement()
 */
 
-const animais = document.querySelector('.animais');
+const animais = document.querySelector(".animais");
 
-const novoH1 = document.createElement('h1');
-novoH1.innerText = 'Novo Título';
-novoH1.classList.add('titulo');
+const novoH1 = document.createElement("h1");
+novoH1.innerText = "Novo Título";
+novoH1.classList.add("titulo");
 
 animais.appendChild(novoH1);
 
@@ -83,13 +83,31 @@ animais.appendChild(novoH1);
 CLONAR ELEMENTOS: Todo elemento selecionado é único. Para criarmos um novo elemento baseado no anterior, é necessário utilizar o método cloneNode()
 */
 
-const titulo1 = document.querySelector('h1');
-const titulo2 = document.querySelector('h1');
+const titulo1 = document.querySelector("h1");
+const titulo2 = document.querySelector("h1");
 const novoTitulo = titulo1;
 // titulo1, titulo2 e novoTitulo são iguais
 
 const cloneTitulo = titulo.cloneNode(true);
-const contato0 = document.querySelector('.contato');
+const contato0 = document.querySelector(".contato");
 contato0.appendChild(cloneTitulo);
 
 // true sinaliza para incluir os filhos
+
+// --------------------------------------------------------------------------------------------
+
+// EXERCÍCIO
+
+// Duplique o menu e adicione ele em copy
+const menu = document.querySelector(".menu");
+const cloneMenu = menu.cloneNode(true);
+const copy = document.querySelector(".copy");
+copy.appendChild(cloneMenu);
+// Selecione o primeiro DT da dl de Faq
+const faq = document.querySelector(".faq-lista");
+const firstDT = faq.querySelector("dt:first-child");
+console.log(firstDT);
+// Selecione o DD referente ao primeiro DT
+const firstDD = firstDT.nextElementSibling;
+console.log(firstDD);
+// Substitua o conteúdo html de .faq pelo de .animais
