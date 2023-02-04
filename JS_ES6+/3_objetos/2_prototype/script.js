cb6845f0d81a533d5ee4eedfaa3cad02265c63fd
adriwco <adriwco@gmail.com>
2023-02-03 23:52:17 -0300

    prototype > exercícios

--- JS_ES6+/3_objetos/2_prototype/script.js
+++ JS_ES6+/3_objetos/2_prototype/script.js
@@ -143,29 +143,39 @@ Carro.marca.charAt(0); // String
 // Deve conter nome, sobrenome e idade
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
-function Pessoas (nome, sobrenome, idade) {
+function Pessoas(nome, sobrenome, idade) {
   this.nome = nome;
   this.sobrenome = sobrenome;
   this.idade = idade;
-  this.nomeCompleto = () => {
-    return `${this.nome} ${this.sobrenome}`;
-  };
+}
+Pessoas.prototype.nomeCompleto = function () {
+  return `${this.nome} ${this.sobrenome}`;
 };
 const nome1 = new Pessoas("Adriano", "Weverton", 22);
+
 // Liste os métodos acessados por
 // dados criados com NodeList,
 // HTMLCollection, Document
+console.log(Object.getOwnPropertyNames(NodeList.prototype));
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
+console.log(Document.prototype);
 
 // Liste os construtores dos dados abaixo
 const li = document.querySelector("li");
-
-li; 
+li;
+console.log(li.constructor.name); // HTMLLIElement
 li.click;
+console.log(li.click.constructor.name); // Function
 li.innerText;
+console.log(li.innerText.constructor.name); // String
 li.value;
+console.log(li.value.constructor.name); // Number
 li.hidden;
+console.log(li.hidden.constructor.name); // Boolean
 li.offsetLeft;
-li.click();
+console.log(li.offsetLeft.constructor.name); // Number
+li.click(); // undefined, não possui esse metodo...
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+li.hidden.constructor.name; // String, pois está a buscar o nome
+console.log(typeof li.hidden.constructor.name);
